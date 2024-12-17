a5734b413003dfe630f7743bab76583f7fb208b8
Mac <mac@rendiero.local>
2024-12-17 23:29:45 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "primary" : ["Lexend", "sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
